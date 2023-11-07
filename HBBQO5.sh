e632067cf0686eed7d824290f84e9c9a6ce08f87
betza <betzabethleon@gmail.com>
2023-11-07 17:30:02 +0100

    Comit 1 Opcio-Menu

--- HBBQO5.sh
+++ HBBQO5.sh
@@ -16,6 +16,34 @@ enDesarrollo() {
   clear
 }
 
+# Función para mostrar el menú de listados de películas
+menuListadosPeliculas() {
+  subopcion=1 
+  clear
+  echo "--------------------------------------------------"
+  echo "1 - Listados de las películas del catálogo."
+  echo "--------------------------------------------------"
+  echo "1-1 Mostrar catálogo alfabético."
+  echo "1-2 Mostrar catálogo cronológico."
+  echo "1-3 Mostrar catálogo por valoración."
+  echo "0 Volver al menú anterior."
+  echo -n "Selecciona una opción: "
+  read subopcion
+}
+
+# Función para mostrar el catálogo alfabético
+Tasca1-1() {
+  clear
+  echo "*************** HBBQO: El Nostre catàleg alfabètic es: ********************"
+  echo "*****************************************************************************"
+
+  # Llama al script de catálogo alfabético (catalogo_alfabetico.sh) para mostrar el catálogo
+  ./Tasca1-1.sh $base_de_datos
+
+  echo "Presiona ENTER para continuar..."
+  read -n 1 -s
+}
+
 
 
 
